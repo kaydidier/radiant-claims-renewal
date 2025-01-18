372823cf1fabd2982d1128efb593ccaade899220
Florien Niyongabo <waka@192.168.1.74>
2025-01-18 02:08:51 +0200

    contact info addintion

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,38 @@
 <footer class="mt-auto">
     <p class="text-center fw-bold fs-5">Contact Us</p>
-    
+
     <div id="footer" class="d-flex flex-column py-4">
         <div>
             <p class="fw-lighter fs-6">
-                <i class="fas fa-phone me-2"></i>
-                +250 7893849849
+                <span class="mx-3">
+                    <i class="fas fa-location me-2"></i>
+                    Nyamirambo
+                </span>
+                <span>
+                    <i class="fas fa-envelope me-2"></i>
+                    info@radiant.rw
+                </span>
             </p>
             <p class="fw-lighter fs-6">
-                <i class="fas fa-envelope me-2"></i>
-                example@domain.com
+                <span class="mx-3">
+                    <i class="fas fa-phone me-2"></i>
+                    +250 788381093
+                </span>
+                <span>
+                    <i class="fas fa-hotline me-2"></i>
+                    Hotline: 2050
+                </span>
             </p>
-            <p class="fw-lighter fs-6">
-                <i class="fab fa-linkedin me-2"></i>
-                <a href="https://www.linkedin.com/in/kundwadidier/" class="text-white text-decoration-none">kundwadidier</a>
 
-                <i class="fab fa-twitter me-2"></i>
-                <a href="https://www.linkedin.com/in/kundwadidier/" class="text-white text-decoration-none">kundwadidier</a>
+            <p class="fw-lighter fs-6">
+                <span class="mx-3">
+                    <i class="fab fa-linkedin me-2"></i>
+                    <a href="https://www.linkedin.com/in/kundwadidier/" class="text-white text-decoration-none">kundwadidier</a>
+                </span>
+                <span>
+                    <i class="fab fa-twitter me-2"></i>
+                    <a href="https://www.linkedin.com/in/kundwadidier/" class="text-white text-decoration-none">kundwadidier</a>
+                </span>
             </p>
         </div>
     </div>
